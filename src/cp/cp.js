bacdbcff6af90d5b461ebe7482cc52ece59add57
Aleksandr <a.teslin90@gmail.com>
2023-06-19 18:03:46 +0300

    refactor: remove unused parts

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -5,13 +5,9 @@ import { fileURLToPath } from "url";
 const DIR_NAME = path.dirname(fileURLToPath(import.meta.url));
 
 const spawnChildProcess = async (args) => {
-  try {
-    const cp = spawn("node", [`${DIR_NAME}/files/script.js`, ...args]);
-    process.stdin.pipe(cp.stdin);
-    cp.stdout.pipe(process.stdout);
-  } catch {
-    throw new Error("Child process failed");
-  }
+  const cp = spawn("node", [`${DIR_NAME}/files/script.js`, ...args]);
+  process.stdin.pipe(cp.stdin);
+  cp.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
